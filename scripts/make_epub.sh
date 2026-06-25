3dc24f2dfcc0d1ba0a5fa24b2882816094bb18f3
kurzs6 <stephan.kurz@oeaw.ac.at>
2026-06-25 10:07:49 +0200

    fixes #1
    quickfix: provide epub files from html output
    
    all credits go to pandoc

---
+++ scripts/make_epub.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+cd manuscript/
+pandoc Perak_Communication_in_the_Age_of_AI.html -o Perak_Communication_in_the_Age_of_AI.epub;
+pandoc Perak_Komunikacija_u_doba_AI.html -o Perak_Komunikacija_u_doba_AI.epub;
